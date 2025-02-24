f866f67958b2e10841dca79da9cdd2dbe3ff80db
Thevidu Sathsara Gamage <thevidu2007627@gmail.com>
2025-02-24 19:14:18 +0530

    Final Project with password hashing using bcrypt

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const db = new pg.Client({
   host: "localhost",
@@ -34,22 +37,32 @@ app.post("/register", async (req, res) => {
   let email = req.body.username;
   let password = req.body.password;
 
-  try {
-    await db.query("INSERT INTO register (email, password) VALUES ($1, $2);", [
-      email,
-      password,
-    ]);
-  } catch (err) {
-    console.log("Email Already Registered");
-  }
+  bcrypt.hash(password, saltRounds, async (err, hash) => {
+    if (err) {
+      console.log("Error while hashing: ", err);
+    }
 
-  res.render("home.ejs");
+    console.log(hash);
+
+    try {
+      await db.query(
+        "INSERT INTO register (email, password) VALUES ($1, $2);",
+        [email, hash]
+      );
+      res.render("home.ejs");
+    } catch (err) {
+      res.send("Email Already Registered");
+    }
+  });
 });
+
+
 
 app.post("/login", async (req, res) => {
   let email = req.body.username;
-  let password = req.body.password;
+  let enteredPassword = req.body.password;
   let loginResult;
+  let hashedPassword;
 
   try {
     loginResult = await db.query("SELECT * FROM register WHERE email = $1", [
@@ -59,15 +72,22 @@ app.post("/login", async (req, res) => {
     console.log("Error while executing the query");
   }
   if (loginResult.rowCount === 0) {
-    console.log("This email is not registered. Please register first!");
-    res.render("home.ejs");
+    res.send("This email is not registered. Please register first!");
   } else if (loginResult.rowCount === 1) {
-    if (password === loginResult.rows[0].password) {
-      res.render("secrets.ejs");
-    } else {
-      console.log("Wrong Email or Password");
-      res.render("login.ejs");
-    }
+
+    hashedPassword = loginResult.rows[0].password
+
+    bcrypt.compare(enteredPassword, hashedPassword, (err,hashResult)=>{
+      if(err){
+        console.log("Error while comparing passwords")
+      }
+      if (hashResult) {
+        res.render("secrets.ejs");
+      } else {
+        res.send("Wrong Email or Password");
+      }
+    })
+
   } else {
     console.log("Server Error");
   }
